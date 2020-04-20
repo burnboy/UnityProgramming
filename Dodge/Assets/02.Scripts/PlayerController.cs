9982986b8b1e0da27b84f177dc1f8131fff81fe8
burnboy <uty1993@naver.com>
2020-04-20 21:41:03 +0900

    컨트롤러
    playercotroller

---
+++ Dodge/Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
