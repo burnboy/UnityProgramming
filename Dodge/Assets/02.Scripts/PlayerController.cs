c1750396b68ba2d2a140409cdd4243a2db8a648f
burnboy <uty1993@naver.com>
2020-04-28 00:10:17 +0900

    방향이동 조금 수정

--- Dodge/Assets/02.Scripts/PlayerController.cs
+++ Dodge/Assets/02.Scripts/PlayerController.cs
@@ -10,12 +10,40 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
