be46141e5df9e635a607f10148ff09661113338e
burnboy <uty1993@naver.com>
2020-04-29 02:22:21 +0900

    총알, 컨트롤러 수정

---
+++ Dodge/Assets/02.Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;//탄알속력
+    private Rigidbody bulletRigidbody;//이동에 사용할 리지드 바디 컴포넌트 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //게임오브젝트에서 rigidbody 컴포넌트를 찾아 bulletrigidbody에 할당 
+        bulletRigidbody = GetComponent<Rigidbody>();
+        //리직드바디의 속도 = 앞쪽방향*이동속력 
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        //3초뒤에 자신의 게임오브젝트 파괴
+        Destroy(gameObject, 3f);
+    }
+
+    //트리거 충돌시 자동으로 실행되는 메서드
+    void OnTriggerEnter(Collider other)
+    {
+        //충돌한 상대방 게임오브젝트가 player태그를 가진경우
+        if (other.tag == "Player")
+        {
+            //상대방 게임오브젝트에서 playercontroller 컴포넌트 가져오기
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            //상대방으로부터 playercontroller 컴포넌트를 가져오는데 성공했다면
+            if (playerController != null)
+            {
+                //상대방 플레이어 컨트롤러 컴포넌트의 Die메서드 실행
+                playerController.Die();
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Dodge/Assets/02.Scripts/PlayerController.cs
+++ Dodge/Assets/02.Scripts/PlayerController.cs
@@ -16,30 +16,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
+        ///////////////////////////////////////////////////////
+        //if (Input.GetKey(KeyCode.UpArrow) == true)
+        //{
+        //    //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+        //    playerRigidbody.AddForce(0f, 0f, speed);
+        //}
 
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
+        //if (Input.GetKey(KeyCode.DownArrow) == true)
+        //{
+        //    //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+        //    playerRigidbody.AddForce(0f, 0f, -speed);
+        //}
 
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
+        //if (Input.GetKey(KeyCode.RightArrow) == true)
+        //{
+        //    //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+        //    playerRigidbody.AddForce(speed, 0f, 0f);
+        //}
 
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        //if (Input.GetKey(KeyCode.LeftArrow) == true)
+        //{
+        //    //위쪽 방향키 입력이 감지된 경우 Z방향힘 주기
+        //    playerRigidbody.AddForce(-speed, 0f, 0f);
+        //}
+        ////////////////////////////////////////////////////////
 
+        //수평축과 수직축의 입력값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        //실제 이동속도를 입력값과 이동속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        //벡터 속도를 xSpeed 0 zSpeed로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        //리지드바디의 속도에 newVellocity 할당
+        playerRigidbody.velocity = newVelocity;
     }
 
     public void Die()
